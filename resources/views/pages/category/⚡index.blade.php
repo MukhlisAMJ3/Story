333cf0f873e109d4a9347c0ffb62cad25368594e
Mukhlis-AMJ <jaelaniabdulmukhlis@gmail.com>
2026-06-20 22:33:03 +0800

    tambahkan file index.blade.php

---
+++ resources/views/pages/category/⚡index.blade.php
@@ -0,0 +1,14 @@
+<?php
+
+use Livewire\Component;
+
+new class extends Component
+{
+    //
+};
+?>
+
+<div>
+    <h1>Selamat Datang di Story Book </h1>
+    {{-- The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk --}}
+</div>
